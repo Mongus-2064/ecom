debe01b385b512cc121600033f64bec40040e031
nAB5N <mongus2064@gmail.com>
2025-08-15 17:46:36 +0545

    Implementing login route

--- controllers/auth/auth.ts
+++ controllers/auth/auth.ts
@@ -15,12 +15,18 @@ export async function POST(req: NextRequest) {
         { status: 400 }
       );
     }
+
     const hashedpassword = await bcrypt.hash(body.password, 10);
     const newUser = new User({ name: body.name, password: hashedpassword });
     await newUser.save();
+
+const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET as string ,{expiresIn: "1h"})
+
+
     return NextResponse.json({
       msg: "User created Successfully",
       user: newUser,
+      token
     });
   } catch (error) {
     return NextResponse.json(
