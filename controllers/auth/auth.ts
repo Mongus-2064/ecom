38f1fdd541226ff15ed8cf0458f7ffe510e917f6
nAB5N <mongus2064@gmail.com>
2025-08-18 13:56:32 +0545

    Edited auth.ts

--- controllers/auth/auth.ts
+++ controllers/auth/auth.ts
@@ -1,36 +1,70 @@
 import { NextResponse, NextRequest } from "next/server";
 import { User } from "@/Models/User";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
-// Login service
+//signup service
 
-export async function POST(req: NextRequest) {
+export async function Signup(req: NextRequest) {
   try {
     const body = await req.json();
 
-    if (!body.name || !body.password) {
+    if (!body.email || !body.password) {
       return NextResponse.json(
-        { error: "Name and password are mandatory!" },
+        { error: "Email and password are mandatory!" },
         { status: 400 }
       );
     }
 
+      //checks if the user is already registered or not in Database
+
+    const user = await User.find(body.email);
+
+    if(user)
+    {
+      return NextResponse.json({error:"Email already in use!"})
+    }
+
     const hashedpassword = await bcrypt.hash(body.password, 10);
-    const newUser = new User({ name: body.name, password: hashedpassword });
+    const newUser = new User({ email: body.email, password: hashedpassword });
     await newUser.save();
 
-const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET as string ,{expiresIn: "1h"})
-
+    const token = jwt.sign(
+      { id: newUser._id },
+      process.env.JWT_SECRET as string,
+      { expiresIn: "1h" }
+    );
 
     return NextResponse.json({
       msg: "User created Successfully",
       user: newUser,
-      token
+      token,
     });
   } catch (error) {
     return NextResponse.json(
       { Error: "Internal Server error", error },
+      { status: 500 }
+    );
+  }
+}
+
+// Login service
+
+export async function Login(req: NextRequest) {
+  try {
+    const body = await req.json();
+
+    if (!body.email || !!body.password) {
+      return NextResponse.json(
+        { error: "Name and password is required" },
+        { status: 400 }
+      );
+    }
+
+  
+  } catch (error) {
+    return NextResponse.json(
+      { Error: "Internal server error", error },
       { status: 500 }
     );
   }
